57dba1d4512e545bc708f0e21aea8accd90351b4
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-03-12 01:17:13 +0300

    snake game most part completed

--- snake.js
+++ snake.js
@@ -12,6 +12,7 @@ const scale = 20;
 const canvaRow = canvaWidth/scale;
 const canvaColumn = canvaHeight/scale;
 console.log(ctx);
+let gameOver = false;
 
 // css Styles
 body.style.backgroundColor = 'lightblue';
@@ -30,10 +31,61 @@ resetBtn.style.cursor = 'pointer';
 
 // Program
 
-let snake = [{
+let snake = {
   x: Math.floor(Math.random()*canvaRow)*scale,
   y: Math.floor(Math.random()*canvaColumn)*scale
-}];
-ctx.fillStyle = snakeColor;
-ctx.strokeStyle = 'white';
-ctx.fillRect(snake[0].x,snake[0].y, scale, scale);
+};
+let food = {
+  x: Math.floor(Math.random()*canvaRow)*scale,
+  y: Math.floor(Math.random()*canvaColumn)*scale
+};
+
+let d ='right';
+let playGame = setInterval(drawSnake, 200);
+body.addEventListener("keydown", changeDirection);
+let hi = setInterval(drawFood,200);
+
+// functions
+function drawSnake(){
+  ctx.clearRect(0,0,canvaWidth,canvaHeight);
+
+    ctx.fillStyle = snakeColor;
+    ctx.strokeStyle = 'white';
+    ctx.fillRect(snake.x,snake.y, scale, scale);
+
+  if (d == 'right')  snake.x +=scale;
+  if (d == 'left') snake.x -=scale;
+  if (d == 'down')  snake.y +=scale;
+  if (d == 'up') snake.y -=scale;
+
+  if(snake.x > canvaWidth) snake.x = 0;
+  else if(snake.x < 0) snake.x = canvaWidth;
+  else if(snake.y > canvaHeight) snake.y = 0;
+  else if(snake.y < 0) snake.y = canvaHeight;
+
+  eat();
+}
+function drawFood(){
+      ctx.fillStyle = foodColor;
+    ctx.fillRect(food.x, food.y, scale, scale);
+}
+function changeDirection(event){
+  console.log(event);
+  if(snake.x>=0 && snake.x<=canvaWidth && snake.y>=0 && snake.y<=canvaHeight){
+  if(event.key === 'ArrowDown') d= 'down';
+  if(event.key === 'ArrowUp') d= 'up';
+  if(event.key === 'ArrowLeft') d= 'left';
+  if(event.key === 'ArrowRight') d= 'right';
+  }
+}
+function eat(){
+  if(snake.x === food.x && snake.y === food.y){
+score.innerHTML = parseInt(score.innerHTML) + 1;
+food.x = Math.floor(Math.random()*canvaRow)*scale;
+food.y = Math.floor(Math.random()*canvaColumn)*scale;
+graw();
+  }
+}
+function graw(){
+  
+}
