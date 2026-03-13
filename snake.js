80f8037b99f1c86ea9b25b53e487dc1712445d20
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-03-13 22:46:18 +0300

    modified

--- snake.js
+++ snake.js
@@ -14,7 +14,7 @@ const canvaColumn = canvaHeight/scale;
 let gameOver = false;
 
 // css Styles
-body.style.backgroundColor = 'lightblue';
+body.style.backgroundImage = 'url("bg.jpg")';
 body.style.textAlign = 'center';
 body.style.margin = '10px';
 canva.style.backgroundColor = 'black';
@@ -37,71 +37,65 @@ let food = {
 let d ='right';
 let playGame = setInterval(drawSnake, 200);
 body.addEventListener("keydown", changeDirection);
-let hi = setInterval(drawFood,0);
 
 // functions
 function drawSnake(){
   ctx.clearRect(0,0,canvaWidth,canvaHeight);
 for (let i = 0; i<snake.length; i++){
-      ctx.fillStyle = snakeColor;
-    ctx.strokeStyle = 'white';
+    ctx.fillStyle = snakeColor;
+    ctx.strokeStyle = 'red';
     ctx.fillRect(snake[i].x,snake[i].y, scale, scale);
+    ctx.strokeRect(snake[i].x,snake[i].y, scale, scale);
 
-    if (d == 'right')  snake[i].x +=scale;
-    if (d == 'left') snake[i].x -=scale;
-    if (d == 'down')  snake[i].y +=scale;
-    if (d == 'up') snake[i].y -=scale;
-    
-    if(snake[i].x > canvaWidth) snake[i].x = 0;
-    else if(snake[i].x < 0) snake[i].x = canvaWidth;
-    else if(snake[i].y > canvaHeight) snake[i].y = 0;
-    else if(snake[i].y < 0) snake[0].y = canvaHeight;
-}
-  eat();
-}
 
-function drawFood(){
-      ctx.fillStyle = foodColor;
+    if(snake[i].x === canvaWidth) snake[i].x = 0;
+    else if(snake[i].x === 0) snake[i].x = canvaWidth;
+    else if(snake[i].y === canvaHeight) snake[i].y = 0;
+    else if(snake[i].y === 0) snake[0].y = canvaHeight;
+}
+    ctx.fillStyle = foodColor;
     ctx.fillRect(food.x, food.y, scale, scale);
-}
 
-function changeDirection(event){
-  console.log(event);
-  
-  for(let i=0; i<snake.length; i++){
-  if(snake[i].x>=0 && snake[i].x<=canvaWidth && snake[i].y>=0 && snake[i].y<=canvaHeight){
-  if(event.key === 'ArrowDown') d= 'down';
-  if(event.key === 'ArrowUp') d= 'up';
-  if(event.key === 'ArrowLeft') d= 'left';
-  if(event.key === 'ArrowRight') d= 'right';
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
+
+    if (d == 'right')  snakeX +=scale;
+    if (d == 'left') snakeX -=scale;
+    if (d == 'down')  snakeY +=scale;
+    if (d == 'up') snakeY -=scale;
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
   }
-}
-}
+  snake.pop();
+  snake.unshift(newHead);
 
-function eat(){
+
   if(snake[0].x === food.x && snake[0].y === food.y){
 score.innerHTML = parseInt(score.innerHTML) + 1;
 food.x = Math.floor(Math.random()*canvaRow)*scale;
 food.y = Math.floor(Math.random()*canvaColumn)*scale;
-
-grow();
-  }
-}
-function grow(){
-  if (d == 'right')  snake[0].x +=scale;
-  if (d == 'left') snake[0].x -=scale;
-  if (d == 'down')  snake[0].y +=scale;
-  if (d == 'up') snake[0].y -=scale;
-
-  let newHead = {
-    x: snake[0].x,
-    y: snake[0].y
-  }
   snake.unshift(newHead);
 
+  }
 }
-/*
-function drawSnake(){
 
+function changeDirection(event){
+  for(let i=0; i<snake.length; i++){
+  if(event.key === 'ArrowDown' && d != 'up') d= 'down';
+  if(event.key === 'ArrowUp'&& d != 'down') d= 'up';
+  if(event.key === 'ArrowLeft' && d!='right') d= 'left';
+  if(event.key === 'ArrowRight' && d!='left') d= 'right';
 }
-*/
+}
+
+
+
+resetBtn.addEventListener("click", function(){
+  score.innerHTML = 0;
+  snake = [{
+    x: Math.floor(Math.random()*canvaRow)*scale,
+    y: Math.floor(Math.random()*canvaColumn)*scale
+  }]
+});
